1b0c7ec099cf3b3f2c5d7e2d7cd929f0738fdb2e
Yoste2904 <joakim.kristoffer.sola.strand@gmail.com>
2023-01-25 13:29:12 +0100

    Perfect

--- Test/Assets/Scripts/MainMenu.cs
+++ Test/Assets/Scripts/MainMenu.cs
@@ -2,10 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static Unity.VisualScripting.Member;
+using TMPro;
+using UnityEngine.UI;
+
 
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame()
+    /*
+    public AudioSource aSource;
+    public Slider mouseSens;
+    public Slider musicVol;
+    */
+    public static float mouseSensetivity = 0.5f;
+    public static float musicVolume = 0.5f;
+
+    public void OnSenseChanged()
+    {
+       // mouseSensetivity = mouseSens.value;
+    }
+
+    public void OnVolumeChange()
+    {
+        //musicVolume = musicVol.value;
+     //   aSource.volume = musicVolume;
+    }
+
+        public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
